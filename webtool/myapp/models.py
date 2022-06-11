bb89c851ff43e1a3bc11e07344b953028ecb4058
Star-Meow <sss37507617@gmail.com>
2022-06-12 00:44:49 +0800

    .

--- webtool/myapp/models.py
+++ webtool/myapp/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+
 
 # Create your models here.
+
+from django.db import models
+
+class TEST(models.Model):
+    ID = models.IntegerField(primary_key=True,)
+    NAME = models.CharField(max_length=255,)
+
+    class Meta:						# 如果讀取已有資料的必要引數！
+        db_table = "Test_Table"
+
+    def __str__(self):
+        return self.NAME
--- webtool/myapp/views.py
+++ webtool/myapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from myapp import forms
+from django.contrib.auth import authenticate
+from django.contrib import auth
+
+def login(request):
+	if request.method == "POST":
+		postform = forms.PostForm(request.POST)
+		if postform.is_valid():  #forms驗證通過
+			username = postform.cleaned_data['username']
+			pd =  postform.cleaned_data['pd']
+			user1 = authenticate(username=username, password=pd)  #管理者驗證
+			if user1 is not None:  #驗證通過
+				auth.login(request, user1)  #登入
+				postform = forms.PostForm()
+				return redirect('/manage/')
+			else:  #驗證未通過
+				message = '登入失敗！'
+		else:
+			message = '驗證碼錯誤！'
+	else:
+		message = '帳號、密碼及驗證碼都必須輸入！'
+		postform = forms.PostForm()
+	return render(request, "login.html", locals())
+
